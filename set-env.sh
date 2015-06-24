d7fe01126bac62b851c073d1446366607afd1c2e
Philipp Müller <mailphilippmuller@gmail.com>
2015-06-24 16:46:36 +0200

    Use conda env instead of custom module. bash script is adjusted, batch script still to do

--- set-env.sh
+++ set-env.sh
@@ -1,12 +1,37 @@
 #!/bin/bash
 # You can make this executable with 'chmod u+x set-env.sh'
 
-python .env/create_modify_env.py $@
-
-OUT=$?
-
 env_name=${PWD##*/}
 
-if [[ ! (OUT -eq 1) ]]; then
+# try to activate environment
+source activate $env_name >> /dev/null 2>&1
+# get return code of activation
+OUT=$?
+
+# create environment if it does not exist or create is supplied
+# this install packages as well
+if [[ ($OUT -eq 1)  || ($1 == "create")]]; then
+    conda env create -n $env_name --file environment.yml
+fi
+
+
+# install packages
+if [[ $1 == "install" ]]; then
+    conda env create -n $env_name --file environment.yml
+fi
+
+# update packages
+if [[ $1 == "update" ]]; then
+    conda env update -n $env_name --file environment.yml
+fi
+
+# check the return code of operations
+OUT=$?
+
+
+if [[ ! ($OUT -eq 1) ]]; then
     source activate $env_name
 fi
+
+# pip install picky >> /dev/null
+
