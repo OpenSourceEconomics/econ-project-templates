0bf41963bad5afe90ea0c9eb885e628f5942b9cf
Philipp Müller <mailphilippmuller@gmail.com>
2015-07-06 14:41:52 +0200

    added picky, use pickys specification files for creation of environments with conda and pip. keep track of version changes when running update with picky

--- set-env.sh
+++ set-env.sh
@@ -10,19 +10,22 @@ OUT=$?
 
 # create environment if it does not exist or create is supplied
 # this install packages as well
-if [[ ($OUT -eq 1)  || ($1 == "create")]]; then
-    conda env create -n $env_name --file environment.yml
-fi
-
-
-# install packages
-if [[ $1 == "install" ]]; then
-    conda env create -n $env_name --file environment.yml
+if [[ ($OUT -eq 1)  || ($1 == "create") || ($1 == "install") ]]; then
+    conda create -n $env_name --file conda_versions.txt
+    if [ -f requirements.txt ]; then
+        pip install -r requirements.txt
+    fi
 fi
 
 # update packages
 if [[ $1 == "update" ]]; then
-    conda env update -n $env_name --file environment.yml
+    conda update --all
+    if [ -f requirements.txt ]; then
+        # Update all pip packages
+        pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
+    fi
+    # Update requirement files
+    picky --update
 fi
 
 # check the return code of operations
@@ -32,6 +35,9 @@ OUT=$?
 if [[ ! ($OUT -eq 1) ]]; then
     source activate $env_name
 fi
+
+# Run picky to test environment consistency
+picky
 
 # pip install picky >> /dev/null
 
