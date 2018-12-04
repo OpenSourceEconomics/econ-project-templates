b8f33ae4d592d53e2e063cdaa4594f3d799217a1
Hans-Martin von Gaudecker <hmgaudecker@gmail.com>
2018-12-04 15:28:29 +0100

    Fix up tests for cookie template.

--- tests/test_cookie.py
+++ tests/test_cookie.py
@@ -1,21 +1,43 @@
 import subprocess
-import os
 import pytest
+
 
 @pytest.fixture
 def basic_project_dict():
     return {
+        "author": "abc",
+        "email": "x@y.z",
+        "affiliation": "Universität Hintertupfingen",
+        "project_name": "Research Project",
+        "project_slug": "research_project",
+        "project_short_description": "Nothing, really",
+        "set_up_git": "n",
+        "git_remote_url": "",
+        "make_initial_commit": "n",
+        "example_to_install": "Python",
         "configure_running_python_from_waf": "y",
         "configure_running_matlab_from_waf": "n",
         "configure_running_r_from_waf": "n",
         "configure_running_stata_from_waf": "n",
         "configure_running_julia_from_waf": "n",
+        "configure_running_sphinx_from_waf": "n",
+        "python_version": "3.7",
+        "add_formatter_to_project": "n",
+        "add_pytest_to_project": "n",
+        "add_tox_to_project": "n",
+        "open_source_license": "n",
+        "_copy_without_render": [
+            ".mywaflib",
+            "*.bib",
+            "src_matlab/library/matlab-json/*",
+            "src_stata/library/stata/ado_ext/*",
+        ],
     }
 
 
-def test_bake_project(cookies,basic_project_dict):
+def test_bake_project(cookies, basic_project_dict):
 
-    result = cookies.bake(extra_context = {**basic_project_dict,**{"project_slug": "research_project"}})
+    result = cookies.bake(extra_context=basic_project_dict)
 
     assert result.exit_code == 0
     assert result.exception is None
@@ -23,37 +45,41 @@ def test_bake_project(cookies,basic_project_dict):
     assert result.project.isdir()
 
 
-def test_project_slug_assertion(cookies,basic_project_dict):
-    result = cookies.bake(extra_context={**basic_project_dict,**{"project_slug": "-"}})
+def test_project_slug_assertion(cookies, basic_project_dict):
+    basic_project_dict["project_slug"] = "-"
+    result = cookies.bake(extra_context=basic_project_dict)
     assert result.exit_code == -1
 
 
-def test_install_Stata_example(cookies,basic_project_dict):
-    result = cookies.bake(extra_context={**basic_project_dict,**{"example_to_install": "Stata"}})
+def test_install_Stata_example(cookies, basic_project_dict):
+    basic_project_dict["example_to_install"] = "Stata"
+    result = cookies.bake(extra_context=basic_project_dict)
     src_estimation_do = result.project.join("src/analysis/first_stage_estimation.do")
     assert result.exit_code == 0
     assert src_estimation_do.check(exists=1)
 
 
-def test_install_Julia_example(cookies,basic_project_dict):
-    result = cookies.bake(extra_context={**basic_project_dict,**{"example_to_install": "Julia"}})
-    schelling_py = result.project.join("src/analysis/schelling.py")
+def test_install_Julia_example(cookies, basic_project_dict):
+    basic_project_dict[
+        "example_to_install"
+    ] = "Julia (Warning: You will need to fix a lot yourself! Patches welcome!)"
+    result = cookies.bake(extra_context=basic_project_dict)
+    get_simulation_draws_jl = result.project.join("src/data_management/get_simulation_draws.jl")
     assert result.exit_code == 0
-    assert schelling_py.check(exists=1)
+    assert get_simulation_draws_jl.check(exists=1)
 
 
-def test_install_run_stata_from_waf(cookies,basic_project_dict):
-    result = cookies.bake(extra_context={**basic_project_dict,**{"configure_running_stata_from_waf": "y"}})
+def test_install_run_stata_from_waf(cookies, basic_project_dict):
+    basic_project_dict["configure_running_stata_from_waf"] = "y"
+    result = cookies.bake(extra_context=basic_project_dict)
     wscript = result.project.join("wscript").read()
     assert 'ctx.load("run_do_script")' in wscript
     assert 'ctx(features="write_project_paths", target="project_paths.do")' in wscript
 
 
-
-
-def test_remove_formatter(cookies,basic_project_dict):
-    result = cookies.bake(extra_context={**basic_project_dict,**{"add_formatter_to_project": "n"}})
-
+def test_remove_formatter(cookies, basic_project_dict):
+    basic_project_dict["add_formatter_to_project"] = "n"
+    result = cookies.bake(extra_context=basic_project_dict)
     formatter = result.project.join("format_python_files.py")
     pyproject = result.project.join("pyproject.toml")
 
@@ -63,7 +89,8 @@ def test_remove_formatter(cookies,basic_project_dict):
 
 
 def test_template_without_sphinx(cookies, basic_project_dict):
-    result = cookies.bake(extra_context={**basic_project_dict,**{"configure_running_sphinx_from_waf": "n"}})
+    basic_project_dict["configure_running_sphinx_from_waf"] = "n"
+    result = cookies.bake(extra_context=basic_project_dict)
 
     documentation_folder = result.project.join("src/documentation")
     wscript = result.project.join("src/wscript").read()
@@ -72,13 +99,9 @@ def test_template_without_sphinx(cookies, basic_project_dict):
     assert "ctx.recurse('documentation')" not in wscript
 
 
-# def test_anaconda_environment_creation(cookies):
-#     result = cookies.bake(extra_context={"create_conda_environment_with_name": "reproducible_research_template"})
-#     env = subprocess.check_output(['conda','env', 'list']).decode()
-#
+# def test_anaconda_environment_creation(cookies, basic_project_dict):
+#     basic_project_dict["create_conda_environment_with_name"] = "reproducible_research_template"
+#     result = cookies.bake(extra_context=basic_project_dict)
+#     env = subprocess.check_output(['conda', 'env', 'list']).decode()
 #     assert 'template_for_reproducible_research' in env
-
-
-# def test_environment_name_assertion(cookies,basic_project_dict):
-#     result = cookies.bake(extra_context={**basic_project_dict,**{"create_conda_environment_with_name": "1- a"}})
-#     assert result.exit_code == -1
+#     # Make sure to remove environment again!
