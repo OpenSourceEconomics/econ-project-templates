a1f647c515e797976d3049af0c16a65bad16bcd5
Hans-Martin von Gaudecker <hmgaudecker@gmail.com>
2019-01-15 12:45:56 +0100

    Remove underscores from author and project name from all potential LaTeX documents. Fixes #49.

--- {{cookiecutter.project_slug}}/src_julia/documentation/index.rst
+++ {{cookiecutter.project_slug}}/src_julia/documentation/index.rst
@@ -1,11 +1,11 @@
-.. This the {{ cookiecutter.project_name }} project's documentation master file.
+.. This the {{ cookiecutter.project_name.replace('_', ' ') }} project's documentation master file.
 
 .. You can adapt this file completely to your liking,
 .. but it should at least contain the `toctree` directive.
 
 
-Welcome to the {{ cookiecutter.project_name }} project's documentation!
-================================================
+Welcome to the {{ cookiecutter.project_name.replace('_', ' ') }} project's documentation!
+================================================================================================
 
 .. toctree::
     :maxdepth: 2
--- {{cookiecutter.project_slug}}/src_r/documentation/conf.py
+++ {{cookiecutter.project_slug}}/src_r/documentation/conf.py
@@ -45,8 +45,8 @@ source_encoding = 'utf-8'
 master_doc = 'index'
 
 # General information about the project.
-project = u'{{ cookiecutter.project_name }}'
-copyright = u'2013-, {{ cookiecutter.author }}'
+project = u'{{ cookiecutter.project_name.replace('_', ' ') }}'
+copyright = u'2019-, {{ cookiecutter.author.replace('_', ' ') }}'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -194,8 +194,8 @@ latex_elements = {
 latex_documents = [(
     'index',
     'project_documentation.tex',
-    'Documentation of the {{ cookiecutter.project_name }} project',
-    '{{ cookiecutter.author }}',
+    'Documentation of the {{ cookiecutter.project_name.replace('_', ' ') }} project',
+    '{{ cookiecutter.author.replace('_', ' ') }}',
     'manual'
 )]
 
