dbc2524dc5c11cb79114be05609154b19ad95f06
Radost Holler <holler@uni-bonn.de>
2019-04-05 21:29:42 +0200

    Fix accidental changes.

--- {{cookiecutter.project_slug}}/format_python_files.py
+++ {{cookiecutter.project_slug}}/format_python_files.py
@@ -12,12 +12,11 @@ def cli():
 
     """
     click.echo("Start reformatting files with isort.")
-    # -sp needed as [isort] in tox.ini in cookie will be read too
-    os.system("isort . -rc -sp tox.ini")
+    os.system("isort . -rc")
     click.echo("Start reformatting files with black.")
     os.system("black .")
     click.echo("Start reformatting files with isort again for commas.")
-    os.system("isort . -rc -sp tox.ini")
+    os.system("isort . -rc")
     click.echo("End reformatting files.")
 
 
