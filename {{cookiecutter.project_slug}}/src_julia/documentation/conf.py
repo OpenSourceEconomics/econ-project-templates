86bf531739b65c85faa788a1934861a211db82f8
Radost <holler@uni-bonn.de>
2019-09-30 17:25:52 +0200

    Reversed faulty changes in julia example.

--- {{cookiecutter.project_slug}}/src_julia/documentation/conf.py
+++ {{cookiecutter.project_slug}}/src_julia/documentation/conf.py
@@ -44,8 +44,8 @@ source_encoding = "utf-8"
 master_doc = "index"
 
 # General information about the project.
-project = "julia example"
-copyright = "2019-, Albert Einstein"
+project = "{{ cookiecutter.project_name.replace('_', ' ') }}"
+copyright = "2019-, {{ cookiecutter.author.replace('_', ' ') }}"
 
 # The version info for the project you"re documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -191,8 +191,8 @@ latex_documents = [
     (
         "index",
         "project_documentation.tex",
-        "Documentation of the julia example project",
-        "Albert Einstein",
+        "Documentation of the { cookiecutter.project_name.replace('_', ' ') }} project",
+        "{{ cookiecutter.author.replace('_', ' ') }}",
         "manual",
     )
 ]
