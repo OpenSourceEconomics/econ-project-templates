3a05def6dc2f5161ed376d09a85579b361159b7d
Radost Holler <holler@uni-bonn.de>
2019-10-30 10:18:16 +0100

    Implemented suggestions from pre-commit.

--- tests/test_pre_commit.py
+++ tests/test_pre_commit.py
@@ -5,14 +5,8 @@ from test_cookie import basic_project_dict
 
 
 def _check_pre_commit(result):
-    b = subprocess.Popen(
-        "pre-commit install",
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        shell=True,
-        cwd=result.project,
-    )
-    b.wait()
+    subprocess.check_output(("pre-commit", "install"), shell=True, cwd=result.project)
+
     c = subprocess.Popen(
         "pre-commit run --all-files",
         stdout=subprocess.PIPE,
