cfad73f2e9a2c218c8229f8df7fc716e4d40c674
Hans-Martin von Gaudecker <hmgaudecker@gmail.com>
2021-01-09 16:10:07 +0100

    Make an actual test for running project.

--- tests/test_run_project.py
+++ tests/test_run_project.py
@@ -3,10 +3,10 @@ import subprocess
 
 def test_pytask_python(cookies, basic_project_dict):
 
-    result = cookies.bake(extra_context=basic_project_dict)
-    subprocess.run("conda develop . && pytask", shell=True, cwd=result.project)
+    cookie_result = cookies.bake(extra_context=basic_project_dict)
+    pytask_result = subprocess.run(
+        "conda develop . && pytask", shell=True, cwd=cookie_result.project
+    )
+    # import pdb; pdb.set_trace()
 
-    assert result.exit_code == 0
-    assert result.exception is None
-    assert result.project.basename == "research_project"
-    assert result.project.isdir()
+    assert pytask_result.check_returncode()
