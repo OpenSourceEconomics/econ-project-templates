a74ce61e0a1cea0337b56744bd31549630becb48
Tobias Raabe <raabe@posteo.de>
2021-01-16 15:08:25 +0100

    fix stata.

--- {{cookiecutter.project_slug}}/src_stata/analysis/task_analysis.py
+++ {{cookiecutter.project_slug}}/src_stata/analysis/task_analysis.py
@@ -26,11 +26,11 @@ specs = [
 @pytask.mark.parametrize(
     "stata, depends_on, produces",
     [
-        [
-            [[str(x) for x in [s["log"], *s["deps"], s["result"]]]],
+        (
+            [str(x) for x in [s["log"], *s["deps"], s["result"]]],
             [s["do"], *s["deps"]],
             [s["log"], s["result"]],
-        ]
+        )
         for s in specs
     ],
 )
