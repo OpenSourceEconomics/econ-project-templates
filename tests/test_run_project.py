dac866a1339d0c9db068f7b97faff2504c964252
Janos Gabler <janos.gabler@gmail.com>
2021-01-18 19:08:27 +0100

    Fix ci (#87)
    * Move to Github Actions for CI

--- tests/test_run_project.py
+++ tests/test_run_project.py
@@ -1,10 +1,10 @@
 import subprocess
 
+import pytest
 from test_cookies import check_cookies_basics
 
 
-def test_python(cookies, basic_project_dict):
-
+def test_pytask_python(cookies, basic_project_dict):
     cookies_result = cookies.bake(extra_context=basic_project_dict)
     check_cookies_basics(cookies_result=cookies_result)
     pytask_result = subprocess.run(
@@ -28,6 +28,7 @@ def test_r(cookies, basic_project_dict):
     assert pytask_result.returncode == 0
 
 
+@pytest.mark.slow
 def test_stata(cookies, basic_project_dict):
 
     basic_project_dict["example_to_install"] = "Stata"
