10765110b64778ff25943e9bb5b28887f02b649c
timmens <tim@mensinger96.com>
2022-01-20 12:14:35 +0100

    Make sure paths are represented coherently

--- {{cookiecutter.project_slug}}/src_python/config.py
+++ {{cookiecutter.project_slug}}/src_python/config.py
@@ -1,5 +1,5 @@
 from pathlib import Path
 
-ROOT = Path(__file__).parent.parent.resolve()
-SRC = Path(__file__).parent.resolve()
+ROOT = Path(__file__).resolve().parent.parent
+SRC = Path(__file__).resolve().parent
 BLD = SRC.joinpath("..", "bld").resolve()
