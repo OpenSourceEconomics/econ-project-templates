ff6141c57892a267d340d1c80f0cd793a62b3791
timmens <tim@mensinger96.com>
2022-02-16 19:36:14 +0100

    Remove CI html build

--- tests/test_cookie.py
+++ tests/test_cookie.py
@@ -107,15 +107,3 @@ def test_check_conda_environment_creation_and_run_all_checks(cookies):
             cwd=result.project_path,
             check=True,
         )
-
-        # Test building documentation
-        subprocess.run(
-            ("conda", "run", "-n", "__test__", "pip", "install", "-e", "."),
-            cwd=result.project_path,
-            check=True,
-        )
-        subprocess.run(
-            ("conda", "run", "-n", "__test__", "make", "html"),
-            cwd=result.project_path / "docs",
-            check=True,
-        )
