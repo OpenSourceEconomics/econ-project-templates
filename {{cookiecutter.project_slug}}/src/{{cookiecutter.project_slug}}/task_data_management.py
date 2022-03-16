7d60b3b6f85f8ca4c400958a514ef76eac658e52
timmens <tim@mensinger96.com>
2022-03-16 13:30:11 +0100

    Fix CI

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/task_data_management.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/task_data_management.py
@@ -5,7 +5,7 @@ from src.data_management import clean_data
 
 
 @pytask.mark.produces(BLD / "data" / "smoking.csv")
-def task_get_smoking_data(produces):
+def task_get_data(produces):
     data_url = "https://www.openintro.org/data/csv/smoking.csv"
     data = pd.read_csv(data_url)
     data.to_csv(produces)
@@ -13,7 +13,7 @@ def task_get_smoking_data(produces):
 
 @pytask.mark.depends_on(BLD / "data" / "smoking.csv")
 @pytask.mark.produces(BLD / "data" / "smoking_clean.csv")
-def task_get_smoking_data(depends_on, produces):
+def task_clean_data(depends_on, produces):
     data = pd.read_csv(depends_on)
     data = clean_data(data)
     data.to_csv(produces)
