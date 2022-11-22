4544c742b3fcc0e2dece7a6bd0edd728b601bc92
Hans-Martin von Gaudecker <hmgaudecker@gmail.com>
2022-11-22 06:35:34 +0100

    Comment out the other test.

--- tests/test_cookie.py
+++ tests/test_cookie.py
@@ -76,47 +76,8 @@ def test_remove_license(cookies):
     assert not license_.exists()
 
 
-@pytest.mark.end_to_end
-def test_check_conda_environment_creation_for_all_examples_and_run_all_checks(cookies):
-    """Test that the conda environment is created and pre-commit passes."""
-    result = cookies.bake(
-        extra_context={
-            "conda_environment_name": "__test__",
-            "make_initial_commit": "yes",
-            "create_conda_environment_at_finish": "yes",
-            "is_ci": "yes",
-        }
-    )
-
-    assert result.exit_code == 0
-    assert result.exception is None
-
-    if sys.platform != "win32":
-        # Switch branch before pre-commit because otherwise failure because on main
-        # branch.
-        subprocess.run(
-            ("git", "checkout", "-b", "test"), cwd=result.project_path, check=True
-        )
-
-        conda_exe = os.environ["CONDA_EXE"]
-
-        # Check linting, but not on the first try since formatters fix stuff.
-        subprocess.run(
-            (conda_exe, "run", "-n", "__test__", "pre-commit", "run", "--all-files"),
-            cwd=result.project_path,
-            check=False,
-            env={},
-        )
-        subprocess.run(
-            (conda_exe, "run", "-n", "__test__", "pre-commit", "run", "--all-files"),
-            cwd=result.project_path,
-            check=True,
-            env={},
-        )
-
-
 # @pytest.mark.end_to_end
-# def test_check_conda_environment_creation_for_python_only_and_run_all_checks(cookies):
+# def test_check_conda_environment_creation_for_all_examples_and_run_all_checks(cookies):
 #     """Test that the conda environment is created and pre-commit passes."""
 #     result = cookies.bake(
 #         extra_context={
@@ -124,9 +85,6 @@ def test_check_conda_environment_creation_for_all_examples_and_run_all_checks(co
 #             "make_initial_commit": "yes",
 #             "create_conda_environment_at_finish": "yes",
 #             "is_ci": "yes",
-#             "add_r_examples": "no",
-#             "add_julia_examples": "no",
-#             "add_stata_examples": "no",
 #         }
 #     )
 
@@ -155,3 +113,45 @@ def test_check_conda_environment_creation_for_all_examples_and_run_all_checks(co
 #             check=True,
 #             env={},
 #         )
+
+
+@pytest.mark.end_to_end
+def test_check_conda_environment_creation_for_python_only_and_run_all_checks(cookies):
+    """Test that the conda environment is created and pre-commit passes."""
+    result = cookies.bake(
+        extra_context={
+            "conda_environment_name": "__test__",
+            "make_initial_commit": "yes",
+            "create_conda_environment_at_finish": "yes",
+            "is_ci": "yes",
+            "add_r_examples": "no",
+            "add_julia_examples": "no",
+            "add_stata_examples": "no",
+        }
+    )
+
+    assert result.exit_code == 0
+    assert result.exception is None
+
+    if sys.platform != "win32":
+        # Switch branch before pre-commit because otherwise failure because on main
+        # branch.
+        subprocess.run(
+            ("git", "checkout", "-b", "test"), cwd=result.project_path, check=True
+        )
+
+        conda_exe = os.environ["CONDA_EXE"]
+
+        # Check linting, but not on the first try since formatters fix stuff.
+        subprocess.run(
+            (conda_exe, "run", "-n", "__test__", "pre-commit", "run", "--all-files"),
+            cwd=result.project_path,
+            check=False,
+            env={},
+        )
+        subprocess.run(
+            (conda_exe, "run", "-n", "__test__", "pre-commit", "run", "--all-files"),
+            cwd=result.project_path,
+            check=True,
+            env={},
+        )
