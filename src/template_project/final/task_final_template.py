5bd09f21a807d8e91b7f8db16ba872e582280a58
Tim Mensinger <mensingertim@gmail.com>
2025-03-05 14:29:53 +0100

    Remove R tasks and update documents

--- src/template_project/final/task_final_template.py
+++ src/template_project/final/task_final_template.py
@@ -1,36 +1,27 @@
 """Tasks running the results formatting (tables, figures)."""
 
 import pandas as pd
-import pyreadr
 import pytask
 
 from template_project.config import BLD, SRC, TEMPLATE_GROUPS
 from template_project.final.plot_template import plot_regression_by_age
 
-for language in ["python", "r"]:
-    extension = "pickle" if language == "python" else "rds"
+for group in TEMPLATE_GROUPS:
 
-    for group in TEMPLATE_GROUPS:
+    @pytask.task(id=group)
+    def task_plot_results_by_age(
+        script=SRC / "final" / "plot_template.py",
+        group=group,
+        predictions_path=BLD / "predictions" / f"{group}.pickle",
+        data_path=BLD / "data" / "stats4schools_smoking.pickle",
+        produces=BLD / "figures" / f"smoking_by_{group}.png",
+    ):
+        """Plot the regression results by age."""
+        data = pd.read_pickle(data_path)
+        predictions = pd.read_pickle(predictions_path)
 
-        @pytask.task(id=f"{group}_{language}")
-        def task_plot_results_by_age(
-            script=SRC / "final" / "plot_template.py",
-            language=language,
-            group=group,
-            predictions_path=BLD / "predictions" / f"{group}.{extension}",
-            data_path=BLD / "data" / f"stats4schools_smoking.{extension}",
-            produces=BLD / "figures" / f"smoking_by_{group}_using_{language}.png",
-        ):
-            """Plot the regression results by age."""
-            if language == "python":
-                data = pd.read_pickle(data_path)
-                predictions = pd.read_pickle(predictions_path)
-            else:
-                data = pyreadr.read_r(data_path)[None]
-                predictions = pyreadr.read_r(predictions_path)[None]
-
-            fig = plot_regression_by_age(data, predictions, group)
-            fig.write_image(produces)
+        fig = plot_regression_by_age(data, predictions, group)
+        fig.write_image(produces)
 
 
 def task_create_results_table(
